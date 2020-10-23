648c47dd9dd98f548fb0cc8517ebc11005080145
MikkoHimanka <mikko.himanka@protonmail.com>
2020-10-23 23:38:29 +0300

    väli

--- library-frontend/src/components/NewBook.js
+++ library-frontend/src/components/NewBook.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react'
 
+import { gql, useMutation } from '@apollo/client'
+
+const CREATE_BOOK = gql`
+  mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(
+      title: $title,
+      author: $author,
+      published: $published,
+      genres: $genres
+    ) {
+      title
+      author
+      published
+      genres
+    }
+  }
+`
+
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
   const [author, setAuhtor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+
+  const [ createBook ] = useMutation(CREATE_BOOK)
 
   if (!props.show) {
     return null
@@ -15,6 +35,7 @@ const NewBook = (props) => {
     event.preventDefault()
     
     console.log('add book...')
+    createBook({ variables: {title, author, published, genres }})
 
     setTitle('')
     setPublished('')
